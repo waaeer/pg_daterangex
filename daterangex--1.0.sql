4d655de431e548998ea86ce71e622bebf04bf1f0
Ivan Panchenko <wao@mail.ru>
2025-02-03 18:24:30 +0300

    added index creation test

--- daterangex--1.0.sql
+++ daterangex--1.0.sql
@@ -1,8 +1,8 @@
 CREATE TYPE daterangex;
-CREATE FUNCTION date_minus(date1 date, date2 date) RETURNS float AS $$
-    SELECT cast(date1 - date2 as float);
-$$ LANGUAGE sql immutable;
 
+CREATE FUNCTION date_minus(date1 date, date2 date) RETURNS float AS $$
+    SELECT cast(date1 - date2 AS float);
+$$ LANGUAGE sql immutable;
 
 CREATE FUNCTION daterangex_canonical(dr daterangex) RETURNS daterangex AS 'MODULE_PATHNAME'
 LANGUAGE C STRICT IMMUTABLE;
--- sql/daterangex.sql
+++ sql/daterangex.sql
@@ -6,3 +6,11 @@ SELECT '[2025-01-01,2025-02-02)'::daterange::daterangex;
 SELECT '[2025-01-01,2025-02-02]'::daterangex::daterange;
 SELECT '[2025-01-01,2025-02-02)'::daterangex::daterange;
 
+CREATE TABLE x (y daterangex);
+INSERT INTO x SELECT daterangex(d::date, (d + '1month'::interval)::date, '[]') FROM generate_series('2015-01-01'::date,'2025-02-02', '1day') d;
+INSERT INTO x SELECT daterangex(d::date, NULL::date, '[)') FROM generate_series('2015-01-01'::date,'2025-02-02', '1day') d;
+SELECT COUNT(*) FROM x;
+CREATE INDEX ON x USING gist (y);
+
+
+
